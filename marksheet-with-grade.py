a3eca5f00d1031f332d72bff896482e336c8e5a5
a-basit92 <108968207+a-basit92@users.noreply.github.com>
2022-07-16 08:57:23 +0500

    Marksheet with Grade

---
+++ marksheet-with-grade.py
@@ -0,0 +1,32 @@
+name = input("Your Name: ")
+fname = input("Father Name: ")
+print("Enter subject marks out of 100.")
+
+english = float(input("English: "))
+urdu = float(input("Urdu: "))
+math = float(input("Mathematics: "))
+physics = float(input("Physics: "))
+chemistry = float(input("Chemistry: "))
+
+totalMarks = english + urdu + math + physics + chemistry
+percentage = (totalMarks / 500) * 100
+
+print ("-------------------------------------------------------------------------")
+print ("|     Student Name: "+(name)+"\t                                          |")
+print ("-------------------------------------------------------------------------")
+print ("|     Father Name: "+(fname)+"\t                                          |")
+print ("-------------------------------------------------------------------------")
+print ("|                               MARK SHEET                              |")
+print ("-------------------------------------------------------------------------")
+print ("|      Subject       |       Total Marks     |      Obtained Marks      |")
+print ("-------------------------------------------------------------------------")
+print ("|      English       |          100          |            " + str(english) + "          |")
+print ("|      Urdu          |          100          |            " + str(urdu) + "          |")
+print ("|      Mathematics   |          100          |            " + str(math) + "          |")
+print ("|      Physics       |          100          |            " + str(physics) + "          |")
+print ("|      Chemistry     |          100          |            " + str(chemistry) + "          |")
+print ("-------------------------------------------------------------------------")
+print ("| Total Obtained Marks: " + str(int(totalMarks)) + "                                              |")
+print ("-------------------------------------------------------------------------")
+print ("| Obtained Percentage: " + str(int(percentage)) + "%                                              |")
+print ("-------------------------------------------------------------------------")
